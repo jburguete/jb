4b86402a425ce279f34ca1de311698560fb89a8e
Javier Burguete <jburguete@eead.csic.es>
2014-10-13 02:57:03 +0200

    Improving comments and reordering XML functions

--- 1.4.5/jb_xml.c
+++ 1.4.5/jb_xml.c
@@ -34,6 +34,7 @@
 #include <string.h>
 #include "jb_xml.h"
 
+#if JB_XML_NODE_GET_FLOAT
 /**
  * \fn JBDOUBLE jb_xml_node_get_float(xmlNode *node, const xmlChar *prop, \
  *   int *error)
@@ -47,7 +48,6 @@
  * \brief error code (1 on success, 0 on error).
  * \return floating number value in JBDOUBLE format.
  */
-#if JB_XML_NODE_GET_FLOAT
 JBDOUBLE jb_xml_node_get_float(xmlNode *node, const xmlChar *prop, int *error)
 {
 	char *buffer;
@@ -63,6 +63,36 @@ JBDOUBLE jb_xml_node_get_float(xmlNode *node, const xmlChar *prop, int *error)
 }
 #endif
 
+#if JB_XML_NODE_GET_FLOAT_WITH_DEFAULT
+/**
+ * \fn JBDOUBLE jb_xml_node_get_float_with_default(xmlNode *node, \
+ *   const xmlChar *prop, int *error, JBDOUBLE def)
+ * \brief Function to get a floating number, in JBDOUBLE format, from a property
+ *   of a XML node or a default value if the node has not the property.
+ * \param node
+ * \brief XML node struct.
+ * \param prop
+ * \brief XML node property.
+ * \param error
+ * \brief error code (1 on success, 0 on error).
+ * \param def
+ * \brief default value.
+ * \return floating number value in JBDOUBLE format.
+ */
+JBDOUBLE jb_xml_node_get_float_with_default
+	(xmlNode *node, const xmlChar *prop, int *error, JBDOUBLE def)
+{
+	if (!xmlHasProp(node, prop))
+	{
+		*error = 1;
+		return def;
+	}
+	return jb_xml_node_get_float(node, prop, error);
+}
+#endif
+
+#if JB_XML_NODE_SET_FLOAT
+
 /**
  * \fn void jb_xml_node_set_float_with_format(xmlNode *node, \
  *   const xmlChar *prop, char *format, JBDOUBLE x)
@@ -77,7 +107,6 @@ JBDOUBLE jb_xml_node_get_float(xmlNode *node, const xmlChar *prop, int *error)
  * \param x
  * \brief floating number value in JBDOUBLE format.
  */
-#if JB_XML_NODE_SET_FLOAT
 void jb_xml_node_set_float_with_format
 	(xmlNode *node, const xmlChar *prop, char *format, JBDOUBLE x)
 {
@@ -102,8 +131,33 @@ void jb_xml_node_set_float(xmlNode *node, const xmlChar *prop, JBDOUBLE x)
 {
 	jb_xml_node_set_float_with_format(node, prop, FWL, x);
 }
+
 #endif
 
+#if JB_XML_NODE_SET_FLOAT_WITH_DEFAULT
+/**
+ * \fn void jb_xml_node_set_float_with_default(xmlNode *node, \
+ *   const xmlChar *prop, JBDOUBLE x, JBDOUBLE def)
+ * \brief Function to set a floating number with default format (see FWL macro)
+ *   in a property of a XML node or none if the number is equal to a default
+ *   value.
+ * \param node
+ * \brief XML node struct.
+ * \param prop
+ * \brief XML node property.
+ * \param x
+ * \brief floating number value in JBDOUBLE format.
+ * \param def
+ * \brief default value.
+ */
+void jb_xml_node_set_float_with_default
+	(xmlNode *node, const xmlChar *prop, JBDOUBLE x, JBDOUBLE def)
+{
+	if (x != def) jb_xml_node_set_float(node, prop, x);
+}
+#endif
+
+#if JB_XML_NODE_GET_INT
 /**
  * \fn long jb_xml_node_get_int(xmlNode *node, const xmlChar *prop, int *error)
  * \brief Function to get an integer number, in long int format, from a property
@@ -116,7 +170,6 @@ void jb_xml_node_set_float(xmlNode *node, const xmlChar *prop, JBDOUBLE x)
  * \brief error code (1 on success, 0 on error).
  * \return integer number value in long int format.
  */
-#if JB_XML_NODE_GET_INT
 long jb_xml_node_get_int(xmlNode *node, const xmlChar *prop, int *error)
 {
 	char *buffer;
@@ -132,6 +185,35 @@ long jb_xml_node_get_int(xmlNode *node, const xmlChar *prop, int *error)
 }
 #endif
 
+#if JB_XML_NODE_GET_INT_WITH_DEFAULT
+/**
+ * \fn long jb_xml_node_get_int_with_default(xmlNode *node, \
+ *   const xmlChar *prop, int *error, long def)
+ * \brief Function to get an integer number, in long int format, from a property
+ *   of a XML node or a default value if the node has not the property.
+ * \param node
+ * \brief XML node struct.
+ * \param prop
+ * \brief XML node property having the number.
+ * \param error
+ * \brief error code (1 on success, 0 on error).
+ * \param def
+ * \brief default value.
+ * \return integer number value in long int format.
+ */
+long jb_xml_node_get_int_with_default
+	(xmlNode *node, const xmlChar *prop, int *error, long def)
+{
+	if (!xmlHasProp(node, prop))
+	{
+		*error = 1;
+		return def;
+	}
+	return jb_xml_node_get_int(node, prop, error);
+}
+#endif
+
+#if JB_XML_NODE_SET_INT
 /**
  * \fn void jb_xml_node_set_int(xmlNode *node, const xmlChar *prop, long x)
  * \brief Function to set an integer number in a property of a XML node.
@@ -142,12 +224,31 @@ long jb_xml_node_get_int(xmlNode *node, const xmlChar *prop, int *error)
  * \param x
  * \brief integer number value in long int format.
  */
-#if JB_XML_NODE_SET_INT
 void jb_xml_node_set_int(xmlNode *node, const xmlChar *prop, long x)
 {
 	char buffer[JB_BUFFER_SIZE];
 	snprintf(buffer,JB_BUFFER_SIZE, "%ld", x);
 	xmlSetProp(node, prop, (xmlChar*)buffer);
+}
+#endif
+
+#if JB_XML_NODE_SET_INT_WITH_DEFAULT
+/**
+ * \fn void jb_xml_node_set_int_with_default(xmlNode *node, \
+ *   const xmlChar *prop, long x, long def)
+ * \brief Function to set an integer number in a property of a XML node or none
+ *   if the number is equal to a default value.
+ * \param node
+ * \brief XML node struct.
+ * \param prop
+ * \brief XML node property.
+ * \param x
+ * \brief integer number value in long int format.
+ */
+void jb_xml_node_set_int_with_default
+	(xmlNode *node, const xmlChar *prop, long x, long def)
+{
+	if (x != def) jb_xml_node_set_int(node, prop, x);
 }
 #endif
 
@@ -157,7 +258,7 @@ JBDOUBLE jb_xml_node_get_time(xmlNode *node, const xmlChar *prop, int *error)
 	char *buffer;
 	JBDOUBLE t;
 	*error = 0;
-	buffer=(char*)xmlGetProp(node, prop);
+	buffer = (char*)xmlGetProp(node, prop);
 	if (!buffer) return 0.;
 	t = jb_get_time(buffer, error);
 	if (*error != 6) *error = 0;
@@ -172,48 +273,6 @@ void jb_xml_node_set_time(xmlNode *node, const xmlChar *prop, JBDOUBLE t)
 	const xmlChar *buffer;
 	buffer = (const xmlChar*)jb_set_time(t);
 	xmlSetProp(node, prop, buffer);
-}
-#endif
-
-#if JB_XML_NODE_GET_FLOAT_WITH_DEFAULT
-JBDOUBLE jb_xml_node_get_float_with_default
-	(xmlNode *node, const xmlChar *prop, int *error, JBDOUBLE def)
-{
-	if (!xmlHasProp(node, prop))
-	{
-		*error = 1;
-		return def;
-	}
-	return jb_xml_node_get_float(node, prop, error);
-}
-#endif
-
-#if JB_XML_NODE_SET_FLOAT_WITH_DEFAULT
-void jb_xml_node_set_float_with_default
-	(xmlNode *node, const xmlChar *prop, JBDOUBLE x, JBDOUBLE def)
-{
-	if (x!=def) jb_xml_node_set_float(node, prop, x);
-}
-#endif
-
-#if JB_XML_NODE_GET_INT_WITH_DEFAULT
-long jb_xml_node_get_int_with_default
-	(xmlNode *node, const xmlChar *prop, int *error, long def)
-{
-	if (!xmlHasProp(node, prop))
-	{
-		*error = 1;
-		return def;
-	}
-	return jb_xml_node_get_int(node, prop, error);
-}
-#endif
-
-#if JB_XML_NODE_SET_INT_WITH_DEFAULT
-void jb_xml_node_set_int_with_default
-	(xmlNode *node, const xmlChar *prop, long x, long def)
-{
-	if (x!=def) jb_xml_node_set_int(node, prop, x);
 }
 #endif
 
@@ -234,7 +293,7 @@ JBDOUBLE jb_xml_node_get_time_with_default
 void jb_xml_node_set_time_with_default
 	(xmlNode *node, const xmlChar *prop, JBDOUBLE t, JBDOUBLE def)
 {
-	if (t!=def) jb_xml_node_set_time(node, prop, t);
+	if (t != def) jb_xml_node_set_time(node, prop, t);
 }
 #endif
 
@@ -243,20 +302,20 @@ FILE* jb_xml_node_get_content_file(xmlNode *node, char **buffer)
 {
 	int l;
 	FILE *file;
-	*buffer=(char*)xmlNodeGetContent(node);
+	*buffer = (char*)xmlNodeGetContent(node);
 	if (!*buffer) return 0;
-	l=strlen(*buffer);
+	l = strlen(*buffer);
 	#if defined(G_OS_WIN32) || defined(BSD)
 		int k;
-		file=g_fopen("node.tmp", "w");
+		file = g_fopen("node.tmp", "w");
 		if (!file) goto exit1;
-		k=fwrite(*buffer, sizeof(char), l, file);
+		k = fwrite(*buffer, sizeof(char), l, file);
 		fclose(file);
-		if (k!=l) goto exit1;
-		file=g_fopen("node.tmp", "r");
+		if (k != l) goto exit1;
+		file = g_fopen("node.tmp", "r");
 exit1:
 	#else
-		file=(FILE*)fmemopen(*buffer, l, "r");
+		file = (FILE*)fmemopen(*buffer, l, "r");
 	#endif
 	if (!file) xmlFree(*buffer);
 	return file;
@@ -268,11 +327,11 @@ void jb_xml_node_set_content_file(xmlNode *node, FILE *file)
 {
 	long int l;
 	char *buffer;
-	l=ftell(file);
-	buffer=(char*)g_malloc(l+1);
+	l = ftell(file);
+	buffer = (char*)g_malloc(l + 1);
 	fseek(file, 0, SEEK_SET);
 	l=fread(buffer, 1, l, file);
-	buffer[l]=0;
+	buffer[l] = 0;
 	xmlNodeSetContent(node, buffer);
 	g_free(buffer);
 }
--- 1.4.5/jb_xml.h
+++ 1.4.5/jb_xml.h
@@ -40,31 +40,15 @@
 	JBDOUBLE jb_xml_node_get_float(xmlNode*, const xmlChar*, int*);
 #endif
 
+#if JB_XML_NODE_GET_FLOAT_WITH_DEFAULT
+	JBDOUBLE jb_xml_node_get_float_with_default
+		(xmlNode*, const xmlChar*, int*, JBDOUBLE);
+#endif
+
 #if JB_XML_NODE_SET_FLOAT
 	void jb_xml_node_set_float_with_format
 		(xmlNode*, const xmlChar*, char*, JBDOUBLE);
 	void jb_xml_node_set_float(xmlNode*, const xmlChar*, JBDOUBLE);
-#endif
-
-#if JB_XML_NODE_GET_INT
-	long jb_xml_node_get_int(xmlNode*, const xmlChar*, int*);
-#endif
-
-#if JB_XML_NODE_SET_INT
-	void jb_xml_node_set_int(xmlNode*, const xmlChar*, long);
-#endif
-
-#if JB_XML_NODE_GET_TIME
-	JBDOUBLE jb_xml_node_get_time(xmlNode*, const xmlChar*, int*);
-#endif
-
-#if JB_XML_NODE_SET_TIME
-	void jb_xml_node_set_time(xmlNode*, const xmlChar*, JBDOUBLE);
-#endif
-
-#if JB_XML_NODE_GET_FLOAT_WITH_DEFAULT
-	JBDOUBLE jb_xml_node_get_float_with_default
-		(xmlNode*, const xmlChar*, int*, JBDOUBLE);
 #endif
 
 #if JB_XML_NODE_SET_FLOAT_WITH_DEFAULT
@@ -72,17 +56,33 @@
 		(xmlNode*, const xmlChar*, JBDOUBLE, JBDOUBLE);
 #endif
 
+#if JB_XML_NODE_GET_INT
+	long jb_xml_node_get_int(xmlNode*, const xmlChar*, int*);
+#endif
+
 #if JB_XML_NODE_GET_INT_WITH_DEFAULT
 	long jb_xml_node_get_int_with_default(xmlNode*, const xmlChar*, int*, long);
+#endif
+
+#if JB_XML_NODE_SET_INT
+	void jb_xml_node_set_int(xmlNode*, const xmlChar*, long);
 #endif
 
 #if JB_XML_NODE_SET_INT_WITH_DEFAULT
 	void jb_xml_node_set_int_with_default(xmlNode*, const xmlChar*, long, long);
 #endif
 
+#if JB_XML_NODE_GET_TIME
+	JBDOUBLE jb_xml_node_get_time(xmlNode*, const xmlChar*, int*);
+#endif
+
 #if JB_XML_NODE_GET_TIME_WITH_DEFAULT
 	JBDOUBLE jb_xml_node_get_time_with_default
 		(xmlNode*, const xmlChar*, int*, JBDOUBLE);
+#endif
+
+#if JB_XML_NODE_SET_TIME
+	void jb_xml_node_set_time(xmlNode*, const xmlChar*, JBDOUBLE);
 #endif
 
 #if JB_XML_NODE_SET_TIME_WITH_DEFAULT
