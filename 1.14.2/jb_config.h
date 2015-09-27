cc0c580a8525659f7395f2567f22bd9b66f6a70e
Javier Burguete <jburguete@eead.csic.es>
2015-09-27 06:10:33 +0200

    Minor improvements in format

--- 1.14.2/jb_config.h
+++ 1.14.2/jb_config.h
@@ -4,14 +4,14 @@
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 	1. Redistributions of source code must retain the above copyright notice,
  * 		this list of conditions and the following disclaimer.
- * 
+ *
  * 	2. Redistributions in binary form must reproduce the above copyright notice,
  * 		this list of conditions and the following disclaimer in the
  * 		documentation and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY Javier Burguete Tolosa ``AS IS'' AND ANY EXPRESS
  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
@@ -202,7 +202,7 @@
  */
 #define JB_XML_NODE_SET_CONTENT_FILE		1
 
-// These macros decide if the corresponding functions have to be inline 
+// These macros decide if the corresponding functions have to be inline
 
 /**
  * \def INLINE_JB_CHANGE
