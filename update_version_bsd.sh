5c8031440c565a1656e5d897b93dd4b441d6d19a
Javier Burguete <jburguete@eead.csic.es>
2020-06-01 02:33:39 +0200

    Updated manual version and BSD update script

--- update_version_bsd.sh
+++ update_version_bsd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gsed -i "s/"$1"\."$2"\."$3"/"$4"\."$5"\."$6"/g" $1.$2.$3/configure.ac $1.$2.$3/Doxyfile $1.$2.$3/locales/*/*/*.po README.*
+gsed -i "s/"$1"\."$2"\."$3"/"$4"\."$5"\."$6"/g" $1.$2.$3/configure.ac $1.$2.$3/Doxyfile $1.$2.$3/locale/*/*/*.po README.*
 git mv $1.$2.$3 $4.$5.$6
 ln -sf $4.$5.$6 jb
