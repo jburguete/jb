4667667ff29ea2f4f85e0163b681befe4b95b14d
Javier Burguete <jburguete@eead.csic.es>
2020-06-02 22:50:41 +0200

    Updated configure and update scripts

--- update_version.sh
+++ update_version.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
-sed -i "s/"$1"\."$2"\."$3"/"$4"\."$5"\."$6"/g" $1.$2.$3/{Doxyfile,configure.ac,locale/*/*/*.po} README.*
+#!/usr/bin/env bash
+if test -x "$(command -v gsed)"; then
+	SED=gsed;
+else
+	SED=sed;
+fi
+$SED -i "s/"$1"\."$2"\."$3"/"$4"\."$5"\."$6"/g" $1.$2.$3/{configure.ac,Doxyfile,locale/*/*/*.po} README.*
 git mv $1.$2.$3 $4.$5.$6
-ln -sf $4.$5.$6 jb
