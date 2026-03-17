101b6f768987e3a33fae7e05110dd1ae30d63741
Javier Burguete <jburguete@eead.csic.es>
2026-03-17 10:53:43 +0100

    Fixed a bug on ARM Neon trigonometric functions

--- 12.1.22/src/math_neon.h
+++ 12.1.22/src/math_neon.h
@@ -7252,18 +7252,13 @@ jbm_4xf32_trig (const float32x4_t x,    ///< float32x4_t vector.
 static inline float32x4_t
 jbm_4xf32_sin (const float32x4_t x)     ///< float32x4_t vector.
 {
+  const int32x4_t v1 = vdupq_n_s32 (1);
   float32x4_t y, s, c;
   int32x4_t q;
   y = jbm_4xf32_trig (x, &q);
-print_float32x4 (stderr, "x", x);
-print_float32x4 (stderr, "y", y);
-print_int32x4 (stderr, "q", q);
   jbm_4xf32_sincoswc (y, &s, &c);
-print_float32x4 (stderr, "s", s);
-print_float32x4 (stderr, "c", c);
-  y = vbslq_f32
-    (vreinterpretq_u32_s32 (vshlq_n_s32 (vandq_s32 (q, vdupq_n_s32 (1)), 31)),
-     c, s);
+  y = vbslq_f32 (vreinterpretq_u32_s32 (vceqq_s32 (vandq_s32 (q, v1), v1)),
+                 c, s);
   return
     vreinterpretq_f32_u32
     (veorq_u32
